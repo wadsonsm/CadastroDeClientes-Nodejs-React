1569635c69a8a8dde293bdfd69c8566faf40563a
wadsonsm <wadsonsm@hotmail.com>
2024-02-16 23:04:27 -0300

    definindo rotas

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const cadastros = [];
 
+app.use(require("cors")());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World os madness');
-});
+app.get('/', (req, res, next) => {
+    res.json(cadastro);
+})
 
-app.listen(3000, console.log('Server started on port 3000'));
+app.post('/cadastro', (req, res, next) => {
+    console.log("Cadastro recebido");
+    cadastro.push({
+        name: req.body.txtNome,
+        idade: parseInt(req.body.txtIdade),
+        uf: req.body.cmbUF
+    });
+    res.json({ message: "Tudo ok por aqui !", dados: cadastros });
+})
+
+app.listen(3000, () => console.log('Server started on port 3000'));
---
+++ frontend/cadastro-front/src/pages/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Form = () => {
+  return (
+    <div>
+          <Header title="React Form" />
+          Olá mundo !
+    </div>
+  )
+}
+
+export default Form;
