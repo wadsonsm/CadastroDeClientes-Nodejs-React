ab73ec7e7644237c92200cd7f909f11c6bded4b7
wadsonsm <wadsonsm@hotmail.com>
2024-02-23 16:53:37 -0300

    gerando gráficos em pdf

--- frontend/cadastro-front/src/pages/Relatorio.js
+++ frontend/cadastro-front/src/pages/Relatorio.js
@@ -1,36 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import ApexChart from 'react-apexcharts';
+import { useReactToPrint } from 'react-to-print';
+import '../App.css';
 
 function Relatorio() {
 
+    const contentDocument = useRef();
+    const handlePrint = useReactToPrint({
+        documentTitle:'Clientes gráfico',
+        content: () => contentDocument.current
+    });
     const [relatorio, setRelatorio] = useState([]);
     //const [series, setSeries] = useState([]);
     const [label, setLabel] = useState([]);
+    const [sigla, setSigla] = useState([]);
 
 
     useEffect(() => {
-        const sigla = [];
+        //const sigla = [];
         const total = [];
         axios.get('http://localhost:3001/relatorio')
             .then(response => {
                 setRelatorio(response.data);
                 response.data.map(item => {
                     console.log(item);
-                    sigla.push(item.uf)
+                    setSigla(item.uf)
                     total.push(item.idade);
                 });
-            verificaIndice(sigla)
+                verificaIndice(sigla)
 
-                
+
             }).catch(err => console.log(err.message));
     }, []);
 
     const series = [44, 55, 13, 43, 22];
     const options = {
         chart: { width: 380, type: 'donut' },
-        labels: Array.from(label)
-    };    
+        labels: Array.from(sigla)
+    };
 
     function verificaIndice(arr) {
         let novaArr = arr.filter((este, i) => arr.indexOf(este) === i);
@@ -39,14 +47,19 @@ function Relatorio() {
 
 
     return (
-        <div>
-            <ApexChart
-                options={options}
-                series={series}
-                type='donut'
-                width={640}
-                height={480}
-            />
+        <div className='container'>
+            <div className='actions'>
+                <button className='btn btn-primary' onClick={handlePrint}>Imprimir</button>
+            </div>
+            <div ref={contentDocument} className='content'>
+                <ApexChart
+                    options={options}
+                    series={series}
+                    type='donut'
+                    width={640}
+                    height={480}
+                />            
+            </div>
         </div>
     )
 }
